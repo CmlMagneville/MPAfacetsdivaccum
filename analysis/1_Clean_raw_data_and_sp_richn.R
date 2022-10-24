55dc67b3f1cbc24aff98b3e7581e67722e30a65f
Cml_Magneville <camille.magneville@gmail.com>
2022-10-24 10:20:38 +0200

    :package: Add data at the hour scale and clean it

--- analysis/1_Clean_raw_data_and_sp_richn.R
+++ analysis/1_Clean_raw_data_and_sp_richn.R
@@ -11,7 +11,7 @@
 ##
 ###############################################################################
 
-
+# A - For dataat the video scale ####
 
 # Step 1: call the 12 tables showing species occurrences in videos ####
 ## two cameras, 6 days / site, 2 sites
@@ -365,4 +365,229 @@ sort(sp_nm_B)
 
 # get species number: 111 species in Fished Area
 length(sp_nm_B)
+
+
+# B - For data at the hour scale ####
+
+
+# Step 1: call the 12 tables showing species occurrences in hours ####
+## two cameras, 6 days / site, 2 sites
+
+presabs_hour_03_A1 <- readRDS(here::here("raw_data", "pres_abs_hour_A1_03.rds"))
+presabs_hour_04_A1 <- readRDS(here::here("raw_data", "pres_abs_hour_A1_04.rds"))
+presabs_hour_05_A1 <- readRDS(here::here("raw_data", "pres_abs_hour_A1_05.rds"))
+presabs_hour_06_A1 <- readRDS(here::here("raw_data", "pres_abs_hour_A1_06.rds"))
+presabs_hour_08_A1 <- readRDS(here::here("raw_data", "pres_abs_hour_A1_08.rds"))
+presabs_hour_09_A1 <- readRDS(here::here("raw_data", "pres_abs_hour_A1_09.rds"))
+
+presabs_hour_03_C2 <- readRDS(here::here("raw_data", "pres_abs_hour_C2_03.rds"))
+presabs_hour_04_C2 <- readRDS(here::here("raw_data", "pres_abs_hour_C2_04.rds"))
+presabs_hour_05_C2 <- readRDS(here::here("raw_data", "pres_abs_hour_C2_05.rds"))
+presabs_hour_06_C2 <- readRDS(here::here("raw_data", "pres_abs_hour_C2_06.rds"))
+presabs_hour_08_C2 <- readRDS(here::here("raw_data", "pres_abs_hour_C2_08.rds"))
+presabs_hour_09_C2 <- readRDS(here::here("raw_data", "pres_abs_hour_C2_09.rds"))
+
+
+###
+
+
+# Step 2 - Remove or merge columns ####
+
+# Remove Chromis_NA and Chelonia_... , Octopus_cyanea, hortulanus_hortulanus (?) columns and ...
+# ... merge A. nigrofuscus + C. striatus + C. binotatus into Ac_Cten_dark ...
+# ... because can not be distinguished on videos ...
+# ... and add Scarus sordidus and Chlorurus sordidus - keep C. sord (same species) ...
+# ... Chlorurus spilurus: delete column as Pacific species (annot error) ...
+# ... Neotrygon kuhlii: delete column (raie pastenague)
+# ... Rename Halichoeres lamarii into Halichoeres marginatus (new name) ...
+# ... Rename Coris variegata into Coris batuensis because variegata is endemic from red sea
+# ... Add Pomacentrus similis and P. caeruleus as P. similis does not belong in Mayotte and ...
+# ... data checked on WAVE
+# ... Aulostomus_maculatus -> Aulostomus chinenesis as A. maculatus only in North America
+
+
+
+# 03 - A1:
+
+sort(colnames(presabs_hour_03_A1))
+presabs_hour_03_A1 <- presabs_hour_03_A1[, which(! colnames(presabs_hour_03_A1) %in% c("Chelonia_mydas"))]
+presabs_hour_03_A1 <- presabs_hour_03_A1[, which(! colnames(presabs_hour_03_A1) %in% c("hortulanus_hortulanus"))]
+
+presabs_hour_03_A1$Ac_Cten_dark <- presabs_hour_03_A1$Ac_Cten_dark + presabs_hour_03_A1$Ctenochaetus_striatus
+presabs_hour_03_A1$Ac_Cten_dark[which(presabs_hour_03_A1$Ac_Cten_dark > 1)] <- 1
+presabs_hour_03_A1 <- presabs_hour_03_A1[, which(! colnames(presabs_hour_03_A1) %in% c("Ctenochaetus_striatus"))]
+
+presabs_hour_03_A1$Chlorurus_sordidus <- presabs_hour_03_A1$Chlorurus_sordidus +
+  presabs_hour_03_A1$Scarus_sordidus
+presabs_hour_03_A1$Chlorurus_sordidus[which(presabs_hour_03_A1$Chlorurus_sordidus > 1)] <- 1
+presabs_hour_03_A1 <- presabs_hour_03_A1[, which(! colnames(presabs_hour_03_A1) %in% c("Scarus_sordidus"))]
+
+
+# 03 - C2:
+
+sort(colnames(presabs_hour_03_C2))
+presabs_hour_03_C2 <- presabs_hour_03_C2[, which(! colnames(presabs_hour_03_C2) %in% c("Chelonia_mydas"))]
+presabs_hour_03_C2 <- presabs_hour_03_C2[, which(! colnames(presabs_hour_03_C2) %in% c("hortulanus_hortulanus"))]
+
+
+# 04 - A1:
+
+sort(colnames(presabs_hour_04_A1))
+presabs_hour_04_A1 <- presabs_hour_04_A1[, which(! colnames(presabs_hour_04_A1) %in% c("Chelonia_mydas"))]
+
+
+# 04 - C2:
+
+sort(colnames(presabs_hour_04_C2))
+presabs_hour_04_C2 <- presabs_hour_04_C2[, which(! colnames(presabs_hour_04_C2) %in% c("Chelonia_mydas"))]
+presabs_hour_04_C2 <- presabs_hour_04_C2[, which(! colnames(presabs_hour_04_C2) %in% c("Chromis_NA"))]
+presabs_hour_04_C2 <- dplyr::rename(presabs_hour_04_C2, "Coris_batuensis" = "Coris_variegata")
+
+presabs_hour_04_C2$Ac_Cten_dark <- presabs_hour_04_C2$Ac_Cten_dark +
+  presabs_hour_04_C2$Ctenochaetus_striatus +
+  presabs_hour_04_C2$Acanthurus_nigrofuscus +
+  presabs_hour_04_C2$Ctenochaetus_binotatus
+presabs_hour_04_C2$Ac_Cten_dark[which(presabs_hour_04_C2$Ac_Cten_dark > 1)] <- 1
+presabs_hour_04_C2 <- presabs_hour_04_C2[, which(! colnames(presabs_hour_04_C2) %in% c("Ctenochaetus_striatus",
+                                                                        "Acanthurus_nigrofuscus",
+                                                                        "Ctenochaetus_binotatus"))]
+
+
+# 05 - A1:
+# Acanthurus tristis has been wrongly annontated (video BI fr 219): ...
+# ... it's in fact A.nigricauda so I add the two columns and remove ...
+# ... A. tristis one:
+# ... and P. similis wrongly annotated -> P. caeruleus (similis not in Mayotte):
+
+sort(colnames(presabs_hour_05_A1))
+presabs_hour_05_A1 <- presabs_hour_05_A1[, which(! colnames(presabs_hour_05_A1) %in% c("Chelonia_mydas"))]
+presabs_hour_05_A1 <- presabs_hour_05_A1[, which(! colnames(presabs_hour_05_A1) %in% c("hortulanus_hortulanus"))]
+
+presabs_hour_05_A1 <- dplyr::rename(presabs_hour_05_A1, "Pomacentrus_caeruleus" = "Pomacentrus_similis")
+
+presabs_hour_05_A1$Ac_Cten_dark <- presabs_hour_05_A1$Ac_Cten_dark +
+  presabs_hour_05_A1$Acanthurus_nigrofuscus +
+  presabs_hour_05_A1$Ctenochaetus_binotatus
+presabs_hour_05_A1$Ac_Cten_dark[which(presabs_hour_05_A1$Ac_Cten_dark > 1)] <- 1
+presabs_hour_05_A1 <- presabs_hour_05_A1[, which(! colnames(presabs_hour_05_A1) %in% c("Acanthurus_nigrofuscus",
+                                                                        "Ctenochaetus_binotatus"))]
+presabs_hour_05_A1$Chlorurus_sordidus <- presabs_hour_05_A1$Chlorurus_sordidus +
+  presabs_hour_05_A1$Scarus_sordidus
+presabs_hour_05_A1$Chlorurus_sordidus[which(presabs_hour_05_A1$Chlorurus_sordidus > 1)] <- 1
+presabs_hour_05_A1 <- presabs_hour_05_A1[, which(! colnames(presabs_hour_05_A1) %in% c("Scarus_sordidus"))]
+
+presabs_hour_05_A1$Acanthurus_nigricauda <- presabs_hour_05_A1$Acanthurus_nigricauda +
+  presabs_hour_05_A1$Acanthurus_tristis
+presabs_hour_05_A1$Acanthurus_nigricauda[which(presabs_hour_05_A1$Acanthurus_nigricauda > 1)] <- 1
+presabs_hour_05_A1 <- presabs_hour_05_A1[, which(! colnames(presabs_hour_05_A1) %in% c("Acanthurus_tristis"))]
+
+
+# 05 - C2:
+
+sort(colnames(presabs_hour_05_C2))
+presabs_hour_05_C2 <- presabs_hour_05_C2[, which(! colnames(presabs_hour_05_C2) %in% c("Chelonia_mydas"))]
+presabs_hour_05_C2 <- presabs_hour_05_C2[, which(! colnames(presabs_hour_05_C2) %in% c("Chlorurus_spilurus"))]
+
+
+presabs_hour_05_C2$Ac_Cten_dark <- presabs_hour_05_C2$Ac_Cten_dark +
+  presabs_hour_05_C2$Ctenochaetus_striatus
+presabs_hour_05_C2$Ac_Cten_dark[which(presabs_hour_05_C2$Ac_Cten_dark > 1)] <- 1
+presabs_hour_05_C2 <- presabs_hour_05_C2[, which(! colnames(presabs_hour_05_C2) %in% c("Ctenochaetus_striatus"))]
+
+
+# 06 - A1
+
+sort(colnames(presabs_hour_06_A1))
+presabs_hour_06_A1$Chlorurus_sordidus <- presabs_hour_06_A1$Chlorurus_sordidus +
+  presabs_hour_06_A1$Scarus_sordidus
+presabs_hour_06_A1$Chlorurus_sordidus[which(presabs_hour_06_A1$Chlorurus_sordidus > 1)] <- 1
+presabs_hour_06_A1 <- presabs_hour_06_A1[, which(! colnames(presabs_hour_06_A1) %in% c("Scarus_sordidus"))]
+
+presabs_hour_06_A1$Aulostomus_chinensis <- presabs_hour_06_A1$Aulostomus_chinensis +
+  presabs_hour_06_A1$Aulostomus_maculatus
+presabs_hour_06_A1$Aulostomus_chinensis[which(presabs_hour_06_A1$Aulostomus_chinensis > 1)] <- 1
+presabs_hour_06_A1 <- presabs_hour_06_A1[, which(! colnames(presabs_hour_06_A1) %in% c("Aulostomus_maculatus"))]
+
+# 06 - C2
+
+sort(colnames(presabs_hour_06_C2))
+# nothing to change
+
+
+# 08 - A1
+
+sort(colnames(presabs_hour_08_A1))
+presabs_hour_08_A1 <- presabs_hour_08_A1[, which(! colnames(presabs_hour_08_A1) %in% c("Chelonia_mydas"))]
+presabs_hour_08_A1 <- presabs_hour_08_A1[, which(! colnames(presabs_hour_08_A1) %in% c("hortulanus_hortulanus"))]
+presabs_hour_08_A1 <- presabs_hour_08_A1[, which(! colnames(presabs_hour_08_A1) %in% c("Octopus_cyanea"))]
+
+
+
+# 08 - C2
+# Scarus caudofasciatus to keep if find a sister species but yet no so remove:
+# ... present in 3 video of this day/cam but no other places
+sort(colnames(presabs_hour_08_C2))
+presabs_hour_08_C2 <- presabs_hour_08_C2[, which(! colnames(presabs_hour_08_C2) %in% c("Chelonia_mydas"))]
+presabs_hour_08_C2 <- presabs_hour_08_C2[, which(! colnames(presabs_hour_08_C2) %in% c("hortulanus_hortulanus"))]
+presabs_hour_08_C2 <- presabs_hour_08_C2[, which(! colnames(presabs_hour_08_C2) %in% c("Scarus_caudofasciatus"))]
+
+
+presabs_hour_08_C2 <- dplyr::rename(presabs_hour_08_C2, Halichoeres_marginatus = Halichoeres_lamarii)
+
+
+# 09 - A1
+# A. tristis has been wrongly annotated, it is in fact Acanthurus tennentii ...
+# ... so I rename the column:
+
+sort(colnames(presabs_hour_09_A1))
+presabs_hour_09_A1 <- presabs_hour_09_A1[, which(! colnames(presabs_hour_09_A1) %in% c("Chelonia_mydas"))]
+presabs_hour_09_A1 <- presabs_hour_09_A1[, which(! colnames(presabs_hour_09_A1) %in% c("Octopus_cyanea"))]
+presabs_hour_09_A1 <- presabs_hour_09_A1[, which(! colnames(presabs_hour_09_A1) %in% c("Neotrygon_kuhlii"))]
+
+
+presabs_hour_09_A1$Ac_Cten_dark <- presabs_hour_09_A1$Ac_Cten_dark +
+  presabs_hour_09_A1$Acanthurus_nigrofuscus +
+  presabs_hour_09_A1$Ctenochaetus_striatus
+presabs_hour_09_A1$Ac_Cten_dark[which(presabs_hour_09_A1$Ac_Cten_dark > 1)] <- 1
+presabs_hour_09_A1 <- presabs_hour_09_A1[, which(! colnames(presabs_hour_09_A1) %in% c("Acanthurus_nigrofuscus",
+                                                                        "Ctenochaetus_striatus"))]
+
+presabs_hour_09_A1 <- dplyr::rename(presabs_hour_09_A1, Acanthurus_tennentii = Acanthurus_tristis)
+
+
+# 09 - C2
+
+sort(colnames(presabs_hour_09_C2))
+presabs_hour_09_C2 <- presabs_hour_09_C2[, which(! colnames(presabs_hour_09_C2) %in% c("Chelonia_mydas"))]
+presabs_hour_09_C2 <- presabs_hour_09_C2[, which(! colnames(presabs_hour_09_C2) %in% c("hortulanus_hortulanus"))]
+presabs_hour_09_C2 <- presabs_hour_09_C2[, which(! colnames(presabs_hour_09_C2) %in% c("Octopus_cyanea"))]
+presabs_hour_09_C2 <- presabs_hour_09_C2[, which(! colnames(presabs_hour_09_C2) %in% c("Neotrygon_kuhlii"))]
+
+presabs_hour_09_C2 <- dplyr::rename(presabs_hour_09_C2, Halichoeres_marginatus = Halichoeres_lamarii)
+presabs_hour_09_C2 <- dplyr::rename(presabs_hour_09_C2, "Coris_batuensis" = "Coris_variegata")
+
+presabs_hour_09_C2$Ac_Cten_dark <- presabs_hour_09_C2$Ac_Cten_dark +
+  presabs_hour_09_C2$Acanthurus_nigrofuscus +
+  presabs_hour_09_C2$Ctenochaetus_binotatus +
+  presabs_hour_09_C2$Ctenochaetus_striatus
+presabs_hour_09_C2$Ac_Cten_dark[which(presabs_hour_09_C2$Ac_Cten_dark > 1)] <- 1
+presabs_hour_09_C2 <- presabs_hour_09_C2[, which(! colnames(presabs_hour_09_C2) %in% c("Ctenochaetus_striatus",
+                                                                        "Acanthurus_nigrofuscus",
+                                                                        "Ctenochaetus_binotatus"))]
+
+
+# save cleaned data:
+saveRDS(presabs_hour_03_A1, here::here("transformed_data", "pres_abs_hour_final_A1_03.rds"))
+saveRDS(presabs_hour_03_C2, here::here("transformed_data", "pres_abs_hour_final_C2_03.rds"))
+saveRDS(presabs_hour_04_A1, here::here("transformed_data", "pres_abs_hour_final_A1_04.rds"))
+saveRDS(presabs_hour_04_C2, here::here("transformed_data", "pres_abs_hour_final_C2_04.rds"))
+saveRDS(presabs_hour_05_A1, here::here("transformed_data", "pres_abs_hour_final_A1_05.rds"))
+saveRDS(presabs_hour_05_C2, here::here("transformed_data", "pres_abs_hour_final_C2_05.rds"))
+saveRDS(presabs_hour_06_A1, here::here("transformed_data", "pres_abs_hour_final_A1_06.rds"))
+saveRDS(presabs_hour_06_C2, here::here("transformed_data", "pres_abs_hour_final_C2_06.rds"))
+saveRDS(presabs_hour_08_A1, here::here("transformed_data", "pres_abs_hour_final_A1_08.rds"))
+saveRDS(presabs_hour_08_C2, here::here("transformed_data", "pres_abs_hour_final_C2_08.rds"))
+saveRDS(presabs_hour_09_A1, here::here("transformed_data", "pres_abs_hour_final_A1_09.rds"))
+saveRDS(presabs_hour_09_C2, here::here("transformed_data", "pres_abs_hour_final_C2_09.rds"))
+
 
